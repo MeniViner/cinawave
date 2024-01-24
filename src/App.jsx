cf0edfccf1c539212d5476cefa8de0bea3e84537
Yehuda Collins <main@Yehudas-MacBook-Air.local>
2024-01-24 21:24:57 +0200

    Describe your change here

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,13 @@ import './card.css';
 import './insideCard.css';
 
 function App() {
-
+  const [originalMovies, setOriginalMovies] = useState([]);
   const [filteredMovies, setFilteredMovies] = useState([]);
 
   useEffect(() => {
     const fetchMovies = async () => {
       const result = await getAllTrendingMovies();
+      setOriginalMovies(result);
       setFilteredMovies(result);
     };
 
@@ -23,19 +24,24 @@ function App() {
   }, []);
 
   const handleSearch = (searchQuery) => {
-    if (!filteredMovies || !Array.isArray(filteredMovies)) {
-        // Handle the case where filteredMovies is not defined or not an array
-        return;
+    if (!originalMovies || !Array.isArray(originalMovies)) {
+      // Handle the case where originalMovies is not defined or not an array
+      return;
     }
 
-    // Use the searchQuery to filter movies
-    const filtered = filteredMovies.filter(movie =>
+    if (searchQuery.trim() === "") {
+      // If searchQuery is empty, reset to the original list
+      setFilteredMovies(originalMovies);
+    } else {
+      // Use the searchQuery to filter movies
+      const filtered = originalMovies.filter(movie =>
         movie.original_title && movie.original_title.toLowerCase().includes(searchQuery.toLowerCase())
-    );
+      );
 
-    // Update the filteredMovies state
-    setFilteredMovies(filtered);
-};
+      // Update the filteredMovies state
+      setFilteredMovies(filtered);
+    }
+  };
 
   return (
     <Router>
@@ -53,34 +59,13 @@ function App() {
 function Home({ filteredMovies }) {
   return (
     <div className="main-cards">
-        {filteredMovies.map((movie) => (
-          <Link key={movie.id} to={`/card/${encodeURIComponent(movie.title)}`}>
-            <Card movie={movie} />
-          </Link>
-        ))}
+      {filteredMovies.map((movie) => (
+        <Link key={movie.id} to={`/card/${encodeURIComponent(movie.title)}`}>
+          <Card movie={movie} />
+        </Link>
+      ))}
     </div>
   );
 }
 
-
 export default App;
-
-
-
-
-// {
-//   "adult": false,
-//   "backdrop_path": "/ruKNVJFViAyR4jguVboFXTskMOe.jpg",
-
-//   "id": 1028703,
-//   "original_language": "en",
-//   "original_title": "The OctoGames",
-//   "overview": "8 contestants compete in 8 deadly, classic children's games. They seek fame beyond their wildest dreams, competing for the chance to take over the YouTube channel of the famous yet elusive masked YouTuber known only as \"JaxPro\".",
-//   "popularity": 960.836,
-//   "poster_path": "/qGz5rffXhegQH5PGUDiObqoOt06.jpg",
-//   "release_date": "2022-10-07",
-//   "title": "The OctoGames",
-//   "video": false,
-//   "vote_average": 4.261,
-//   "vote_count": 23
-// }
