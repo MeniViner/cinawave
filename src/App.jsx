11b5b62a00fa219c6268d152e82a984f333bef00
YehudaCollins <126301281+YehudaCollins@users.noreply.github.com>
2024-01-24 21:41:13 +0200

    הקוד עכשיו עובד רק לפי מילים ששלמות (ולא פשוט אותיות  קיימות)

--- src/App.jsx
+++ src/App.jsx
@@ -28,20 +28,22 @@ function App() {
       // Handle the case where originalMovies is not defined or not an array
       return;
     }
-
+  
     if (searchQuery.trim() === "") {
       // If searchQuery is empty, reset to the original list
       setFilteredMovies(originalMovies);
     } else {
-      // Use the searchQuery to filter movies
+      // Use the searchQuery to filter movies with exact word match
       const filtered = originalMovies.filter(movie =>
-        movie.original_title && movie.original_title.toLowerCase().includes(searchQuery.toLowerCase())
+        movie.original_title && movie.original_title.toLowerCase() === searchQuery.toLowerCase()
       );
-
+      
+  
       // Update the filteredMovies state
       setFilteredMovies(filtered);
     }
   };
+  
 
   return (
     <Router>
