9dbd35743f2ee4007cfcc347c4dd7c477f2a9b34
Yehuda Collins <main@Yehudas-MacBook-Air.local>
2024-01-24 16:33:19 +0200

    Ysssssssssssssse

--- src/insideCard.jsx
+++ src/insideCard.jsx
@@ -1,12 +1,13 @@
+// InsideCard.js
+
 import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
-import movies from "./moviesData";
 
-function InsideCard() {
+function InsideCard({ filteredMovies }) {
   const [like, setLike] = useState(0);
   const [disLike, setDislike] = useState(0);
   const { insideCard } = useParams();
-  const selectedMovie = movies.find(movie => movie.name === decodeURIComponent(insideCard));
+  const selectedMovie = filteredMovies.find(movie => movie.title === decodeURIComponent(insideCard));
 
   if (!selectedMovie) {
     return <div>Movie not found</div>;
@@ -14,19 +15,20 @@ function InsideCard() {
 
   return (
     <div className="insideCard">
-          <img className="background-image"
-                src={`https://image.tmdb.org/t/p/w500/${selectedMovie.backdrop_path}`}
-                alt={selectedMovie.title}
-            />
-        <div className='info'>   
-            <h1>{selectedMovie.name}</h1>
+      <img
+        className="background-image"
+        src={`https://image.tmdb.org/t/p/w500/${selectedMovie.backdrop_path}`}
+        alt={selectedMovie.title}
+      />
+      <div className='info'>
+         <h1>{selectedMovie.title}</h1>
             <h2>Overview: {selectedMovie.overview}</h2>
-            <p>{selectedMovie.genres}</p>
+            {/* <p>{selectedMovie.genres}</p>
             <p>{selectedMovie.country}</p>
             <p>{selectedMovie.duration}</p>
             <p>{selectedMovie.production}</p>
             <p>{selectedMovie.release}</p>
-            <p>IMDB: {selectedMovie.imdb}</p>
+            <p>IMDB: {selectedMovie.imdb}</p> */}
             <br></br><br></br><br></br><br></br>
             <div className="likes">
                 <img
@@ -47,8 +49,7 @@ function InsideCard() {
                 <div className="text-like">{disLike}</div>
             </div>
         </div>
-            
-    </div>
+      </div>
   );
 }
 
