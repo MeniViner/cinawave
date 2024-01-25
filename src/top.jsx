c01327de53b3726c969fd6a512e70da9519f60cb
YehudaCollins <ycollins555@gmail.com>
2024-01-26 00:53:34 +0200

    .

--- src/top.jsx
+++ src/top.jsx
@@ -7,45 +7,42 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 library.add(faMagnifyingGlass, faSearch, faBarsStaggered);
 
-
 function Top({ onSearch }) {
     const [searchQuery, setSearchQuery] = useState("");
     const handleSearch = () => {onSearch(searchQuery);};
     const backgroundImageStyle = {
-      backgroundImage: `url("./images/300.jpeg")`, // Update the path to your image
-      backgroundSize: 'cover',
-      backgroundPosition: 'center',
+        backgroundImage: `url("./images/300.jpeg")`, // Update the path to your image
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
     };
     const handleKeyDown = (event) => {
         if (event.key === 'Enter') {
             handleSearch();
         }
     };
-     
+
     return (
-       <div className="main-main" style={backgroundImageStyle}>
+        <div className="main-main" style={backgroundImageStyle}>
             <div className="overlay"></div>
-                <div className="up">
-                    <br></br>
-                    <div class="titel"> 
-                        <img className="logo" src="./images/logo.png" alt="logo" width={"20%"}/>
-                        <div className="search-box">
-                            <FontAwesomeIcon icon={faSearch} size="lg "/>
-                            <input
+            <div className="up">
+                <br></br>
+                <div className="titel"> 
+                    <img className="logo" src="./images/logo.png" alt="logo" width={"20%"}/>
+                    <div className="search-box">
+                        <FontAwesomeIcon icon={faSearch} size="lg "/>
+                        <input
                             className="search-bar"
                             type="text"                
                             placeholder="Search..."
-                            
                             value={searchQuery}
                             onChange={(e) => setSearchQuery(e.target.value)}
                             style={{ border: 'none' }}  
                             onKeyDown={handleKeyDown} // Call handleKeyDown on key press
                         /> 
-                        {/* <button onClick={handleSearch}>Search</button> */}
                     </div>
-                        <div className="burger-menu right-align" >
-                            <FontAwesomeIcon icon={faBarsStaggered} size="lg" id="burger-menu"/>        
-                        </div>
+                    <div className="burger-menu right-align" >
+                        <FontAwesomeIcon icon={faBarsStaggered} size="lg" id="burger-menu"/>        
+                    </div>
                 </div>
                 <div className="content">
                     <div className="main">
@@ -62,8 +59,8 @@ function Top({ onSearch }) {
                     </div>
                 </div>
             </div>
-      </div>
-  );
+        </div>
+    );
 }
 
 export default Top;
