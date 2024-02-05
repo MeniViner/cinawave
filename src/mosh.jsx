e608cbdf11376ca2124023cda8f4bc1c5fec68f5
YehudaCollins <ycollins555@gmail.com>
2024-02-05 15:44:24 +0200

    .

---
+++ src/mosh.jsx
@@ -0,0 +1,56 @@
+// import React, { useState, createContext, useContext } from 'react';
+// import './Mosh.css';
+
+// const UserContext = createContext();
+
+// function Mosh() {
+//     const [totalLikes, setTotalLikes] = useState(0);
+
+//     const handleLike = () => {
+//         setTotalLikes(totalLikes + 1);
+//     };
+
+//     const handleDislike = () => {
+//         setTotalLikes(totalLikes - 1);
+//     };
+
+//     return (
+//         <UserContext.Provider value={{ totalLikes, handleLike, handleDislike }}>
+//             <div className="main">
+//                 <Button type="like" />
+//                 <PrintSum />
+//                 <Button type="dislike" />
+//             </div>
+//         </UserContext.Provider>
+//     );
+// }
+
+// function Button({ type }) {
+//     const { handleLike, handleDislike } = useContext(UserContext);
+
+//     const handleClick = () => {
+//         if (type === 'like') {
+//             handleLike();
+//         } else if (type === 'dislike') {
+//             handleDislike();
+//         }
+//     };
+
+//     return (
+//         <div>
+//             <button className='button' onClick={handleClick}>{type === 'like' ? 'Like' : 'Dislike'}</button>
+//         </div>
+//     );
+// }
+
+// function PrintSum() {
+//     const { totalLikes } = useContext(UserContext);
+
+//     return (
+//         <div>
+//             <div className='text'>{totalLikes}</div>
+//         </div>
+//     );
+// }
+
+// export default Mosh;
